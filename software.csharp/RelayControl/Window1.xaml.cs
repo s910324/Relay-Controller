d556ddb82f79a05848c6f128758f3ec2d4fa70f1
s910324 <scott910324@gmail.com>
2020-01-04 22:23:02 +0800

    update control software

--- software.csharp/RelayControl/Window1.xaml.cs
+++ software.csharp/RelayControl/Window1.xaml.cs
@@ -29,23 +29,57 @@ namespace RelayControl
 		public Window1()
 		{
 			InitializeComponent();
-			ManagementClass processClass = new ManagementClass("Win32_PnPEntity");
+			
+			InitializeUserInterface();
 
-
-            ManagementObjectCollection Ports = processClass.GetInstances();
-            string device = "No recognized";
-            foreach (ManagementObject property in Ports)
-            {
-                if (property.GetPropertyValue("Name") != null)
-                    if (property.GetPropertyValue("Name").ToString().Contains("USB") &&
-                        property.GetPropertyValue("Name").ToString().Contains("COM"))
-                    {
-                        Trace.WriteLine(property.GetPropertyValue("Name").ToString());
-                        device = property.GetPropertyValue("Name").ToString();
-                    }
-
-                    }
 			
 		}
+		
+	
+		public void InitializeUserInterface(){
+			ComboBox serial_select = new ComboBox();
+			Grid     main_grid     = new Grid();
+			main_grid.ColumnDefinitions.Add(new ColumnDefinition());
+			main_grid.ColumnDefinitions.Add(new ColumnDefinition());
+			main_grid.RowDefinitions.Add(new RowDefinition());
+			main_grid.RowDefinitions.Add(new RowDefinition());
+			
+			Grid.SetRow(serial_select, 0);
+			Grid.SetColumn(serial_select, 0);
+			main_grid.Children.Add(serial_select);
+			List<ComboData> devices = GetSerialDevices();
+			serial_select.ItemsSource = devices;
+			serial_select.DisplayMemberPath = "Value";
+			serial_select.SelectedValuePath = "Id";
+							
+			this.Content = main_grid;
+		}
+   
+   
+		public List<ComboData> GetSerialDevices(){
+			ManagementClass            processClass = new ManagementClass("Win32_PnPEntity");
+            ManagementObjectCollection ports        = processClass.GetInstances();
+            List<ComboData>            ListData     = new List<ComboData>();          
+     
+            int i = 0;
+            foreach(ManagementObject property in ports){                    
+                if (property.GetPropertyValue("Name") != null)
+                    if (property.GetPropertyValue("Name").ToString().Contains("USB") && property.GetPropertyValue("Name").ToString().Contains("COM")){
+                		string name = property.GetPropertyValue("Name").ToString();
+                        Trace.WriteLine(name);
+                        ListData.Add(new ComboData { Id = i, Value = name });
+                        Trace.WriteLine(i);
+                        i++;
+                    }						
+            }
+            return ListData;
+		}
 	}
+	
+	public class ComboData{ 
+		public int Id { get; set; } 
+		public string Value { get; set; } 
+	}
+ 
+
 }
