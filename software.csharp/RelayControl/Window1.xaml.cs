c58dd3ddcd1c2c0d28ad9b9421f0416dd5c4f128
s910324 <scott910324@gmail.com>
2020-01-05 01:51:48 +0800

    update bual rate selection

--- software.csharp/RelayControl/Window1.xaml.cs
+++ software.csharp/RelayControl/Window1.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.IO.Ports;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace RelayControl
 {
@@ -38,6 +39,7 @@ namespace RelayControl
 	
 		public void InitializeUserInterface(){
 			ComboBox serial_select = new ComboBox();
+			ComboBox bual_select   = new ComboBox();
 			Grid     main_grid     = new Grid();
 			main_grid.ColumnDefinitions.Add(new ColumnDefinition());
 			main_grid.ColumnDefinitions.Add(new ColumnDefinition());
@@ -47,28 +49,56 @@ namespace RelayControl
 			Grid.SetRow(serial_select, 0);
 			Grid.SetColumn(serial_select, 0);
 			main_grid.Children.Add(serial_select);
-			List<ComboData> devices = GetSerialDevices();
+			
+			Grid.SetRow(bual_select, 0);
+			Grid.SetColumn(bual_select, 1);
+			main_grid.Children.Add(bual_select);
+			
+			List<PortComboData> devices  = GetSerialDevices();
 			serial_select.ItemsSource = devices;
-			serial_select.DisplayMemberPath = "Value";
+			serial_select.DisplayMemberPath = "Display";
 			serial_select.SelectedValuePath = "Id";
-							
+			
+			List<BualComboData> bualrates = new List<BualComboData>();
+			bual_select.ItemsSource = bualrates;
+			bual_select.DisplayMemberPath = "Display";
+			bual_select.SelectedValuePath = "Id";
+			
+			int i = 0;
+			foreach (int rate in new int[] {2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600}){
+				bualrates.Add( new BualComboData{
+					Id      = i,
+					Bual    = rate,
+					Status  = "",
+					Display = "   "+ rate
+				});
+			}
+			
 			this.Content = main_grid;
 		}
    
    
-		public List<ComboData> GetSerialDevices(){
+		public List<PortComboData> GetSerialDevices(){
 			ManagementClass            processClass = new ManagementClass("Win32_PnPEntity");
             ManagementObjectCollection ports        = processClass.GetInstances();
-            List<ComboData>            ListData     = new List<ComboData>();          
+            List<PortComboData>        ListData     = new List<PortComboData>();          
      
             int i = 0;
             foreach(ManagementObject property in ports){                    
                 if (property.GetPropertyValue("Name") != null)
-                    if (property.GetPropertyValue("Name").ToString().Contains("USB") && property.GetPropertyValue("Name").ToString().Contains("COM")){
-                		string name = property.GetPropertyValue("Name").ToString();
-                        Trace.WriteLine(name);
-                        ListData.Add(new ComboData { Id = i, Value = name });
-                        Trace.WriteLine(i);
+                	if (property.GetPropertyValue("Name").ToString().Contains("USB") && property.GetPropertyValue("Name").ToString().Contains("COM")){
+                		string c_name    = property.GetPropertyValue("Name").ToString();
+						Match  match     = Regex.Match(c_name, @"([\w|\W]+)\((COM([0-9])+)\)");
+			   
+						if (match.Success){
+							ListData.Add(new PortComboData { 
+				             	Id      = i, 
+				             	Name    = match.Groups[1].Value, 
+				             	COM     = match.Groups[2].Value,
+				             	Display = String.Format("[{0}] -- {1}", match.Groups[2].Value, match.Groups[1].Value)
+							});
+							Trace.WriteLine(String.Format("index: {0} name: {1} COM: {2}", i, match.Groups[1].Value, match.Groups[2].Value));                                                                      
+						}
                         i++;
                     }						
             }
@@ -76,10 +106,18 @@ namespace RelayControl
 		}
 	}
 	
-	public class ComboData{ 
-		public int Id { get; set; } 
-		public string Value { get; set; } 
+	public class PortComboData{ 
+		public int    Id      { get; set; } 
+		public string Name    { get; set; }
+		public string COM     { get; set; }
+		public string Display { get; set; }
 	}
- 
+	
+	public class BualComboData{ 
+		public int    Id      { get; set; } 
+		public int    Bual    { get; set; }
+		public string Status  { get; set; }
+		public string Display { get; set; }
+	} 
 
 }
